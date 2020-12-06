172c79e29f31ea0ed44c8250b88f6b042589abf4
phaneesh <paneeshhj@gmail.com>
2020-12-07 01:52:32 +0530

    replace tabs to spaces for better formatting

--- day-3/day-3-1.py
+++ day-3/day-3-1.py
@@ -10,11 +10,11 @@ line_len = len(lines[0].replace('\n', ''))
 tree_counter = 0
 # print(line_len)
 for line in lines:
-	# print(len(line.replace('\n', '')))
-	# print(curr_pos)
-	if line[curr_pos] == '#':
-		tree_counter += 1
-	counter += 3
-	curr_pos = counter % line_len
+    # print(len(line.replace('\n', '')))
+    # print(curr_pos)
+    if line[curr_pos] == '#':
+        tree_counter += 1
+    counter += 3
+    curr_pos = counter % line_len
 
 print(tree_counter)
--- day-3/day-3-2.py
+++ day-3/day-3-2.py
@@ -2,33 +2,33 @@ f = open('../inputs/day-3.txt', 'r')
 lines = f.readlines()
 
 def get_trees(r, d):
-	f = open('../inputs/day-3.txt', 'r')
-	lines = f.readlines()
+    f = open('../inputs/day-3.txt', 'r')
+    lines = f.readlines()
 
-	curr_pos = 0
-	counter = 0
+    curr_pos = 0
+    counter = 0
 
-	right = r
-	down = d
+    right = r
+    down = d
 
-	line_len = len(lines[0].replace('\n', ''))
-	tree_counter = 0
+    line_len = len(lines[0].replace('\n', ''))
+    tree_counter = 0
 
-	for line in lines[::down]:
-		if line[curr_pos] == '#':
-			tree_counter += 1
-		counter += right
-		curr_pos = counter % line_len
+    for line in lines[::down]:
+        if line[curr_pos] == '#':
+            tree_counter += 1
+        counter += right
+        curr_pos = counter % line_len
 
-	print(tree_counter)
+    print(tree_counter)
 
-	return tree_counter
+    return tree_counter
 
 
 directions = [(1,1), (3,1), (5,1), (7,1), (1,2)]
 product = 1
 
 for i in directions:
-	product *= get_trees(i[0], i[1])
+    product *= get_trees(i[0], i[1])
 
 print(product)
